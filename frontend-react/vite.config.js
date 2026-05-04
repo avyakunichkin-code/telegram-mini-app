a336bd1456a602b6ba200987246d236a70adaa04
Aleksei Iakunichkin <avyakunic1@mts.ru>
2026-05-04 21:02:44 +0300

    настройка деплоя

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/telegram-mini-app/',  // <-- имя репозитория
   server: {
     proxy: {
       '/api': {
         target: 'https://telegram-mini-app-zwfs.onrender.com',
         changeOrigin: true,
-        secure: false,   // разрешаем самоподписанные сертификаты (если нужно)
+        secure: false,
       }
     }
   }
